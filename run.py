0b38d73cbf5732086b5906127b9d2b24b34058e3
DionneMaguire <dionnemaguire@yahoo.com>
2022-03-28 21:52:50 +0000

    add requirements for deployment

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -25,7 +24,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
         
         sales_data = data_str.split(",")
 
@@ -37,7 +36,7 @@ def get_sales_data():
 
 def validate_data(values):
     """
-    inside thr try, converts all string values into integers.
+    inside the try, converts all string values into integers.
     Raises ValueError if string cannot be converted into int,
     or if there aren't exactly 6 values.
     """
